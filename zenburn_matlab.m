6d20ae3be2e360b46e6a2c9f23717de4e375080e
Cameron Fackler <cfackler@gmail.com>
2012-05-03 22:35:03 -0400

    Initial commit. Add color scheme M-file.

---
+++ zenburn_matlab.m
@@ -0,0 +1,36 @@
+% Zenburn Color Theme for MATLAB
+% Ported to MATLAB by Cameron Fackler, cfackler at gmail dot com
+% 
+% For more information, see http://slinky.imukuppi.org/zenburnpage/
+
+com.mathworks.services.Prefs.setBooleanPref('ColorsUseSystem', 0);
+
+com.mathworks.services.Prefs.setColorPref('ColorsText', java.awt.Color(int32(220), int32(220), int32(204)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('ColorsText');
+
+com.mathworks.services.Prefs.setColorPref('ColorsBackground', java.awt.Color(int32(63), int32(63), int32(63)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('ColorsBackground');
+
+com.mathworks.services.Prefs.setColorPref('Colors_M_Keywords', java.awt.Color(int32(240), int32(223), int32(175)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('Colors_M_Keywords');
+
+com.mathworks.services.Prefs.setColorPref('Colors_M_Comments', java.awt.Color(int32(127), int32(159), int32(127)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('Colors_M_Comments');
+
+com.mathworks.services.Prefs.setColorPref('Colors_M_Strings', java.awt.Color(int32(204), int32(147), int32(147)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('Colors_M_Strings');
+
+com.mathworks.services.Prefs.setColorPref('Colors_M_UnterminatedStrings', java.awt.Color(int32(236), int32(147), int32(211)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('Colors_M_UnterminatedStrings');
+
+com.mathworks.services.Prefs.setColorPref('Colors_M_SystemCommands', java.awt.Color(int32(239), int32(239), int32(143)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('Colors_M_SystemCommands');
+
+com.mathworks.services.Prefs.setColorPref('Colors_M_Errors', java.awt.Color(int32(241), int32(140), int32(150)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('Colors_M_Errors');
+
+com.mathworks.services.Prefs.setColorPref('Colors_HTML_HTMLLinks', java.awt.Color(int32(140), int32(208), int32(211)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('Colors_HTML_HTMLLinks');
+
+com.mathworks.services.Prefs.setColorPref('ColorsMLintAutoFixBackground', java.awt.Color(int32(38), int32(38), int32(38)));
+com.mathworks.services.ColorPrefs.notifyColorListeners('ColorsMLintAutoFixBackground');
